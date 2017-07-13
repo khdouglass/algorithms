b99304270e53ee395e61435e36f062a4ed9f410f
vagrant <khdouglass@gmail.com>
2017-07-13 03:54:28 +0000

    Improved algorithm.

--- bracket_validator.py
+++ bracket_validator.py
@@ -12,7 +12,35 @@ def bracket_validator(equation):
                 bracket_list.pop()
             else:
                 return False
-    if len(bracket_list) == 0:
-        return True
-    else: 
-        return False
+
+    return bracket_list == []
+
+def is_valid(code):
+    """Interview Cake solution."""
+
+    openers_to_closers = {
+        '(' : ')',
+        '{' : '}',
+        '[' : ']'
+    }
+
+    openers = frozenset(openers_to_closers.keys())
+    closers = frozenset(openers_to_closers.values())
+
+    openers_stack = []
+
+    for char in code:
+        if char in openers:
+            openers_stack.append(char)
+        elif char in closers:
+            if not openers_stack:
+                return False
+            else:
+                last_unclosed_opener = openers_stack.pop()
+
+                # if this closer doesn't correspond to the most recently
+                # seen unclosed opener, short-circuit, returning false
+                if not openers_to_closers[last_unclosed_opener] == char:
+                    return False
+
+    return openers_stack == []
